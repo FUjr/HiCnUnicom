ae7f70d33238476e44b616392b4363e8f406f682
FUjr <53910283+FUjr@users.noreply.github.com>
2021-06-26 03:13:25 -0700

    Update CnUnicom.sh
    add qmsg

--- CnUnicom.sh
+++ CnUnicom.sh
@@ -1,4 +1,4 @@
-curl https://qmsg.zendee.cn/send/$skey?msg=$
+curl https://qmsg.zendee.cn/send/$skey?msg=$parameter
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin && export PATH
 # Usage:
